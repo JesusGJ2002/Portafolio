a29beacd0ef8c33b6dc807e11791094fc308e650
unknown <jesusalvis42gmail.com>
2025-07-07 03:40:53 -0500

    cambio en el nombre de la pestaña

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Portafolio',
+  description: 'Created with v0 & JesusGJ',
   generator: 'v0.dev',
 }
 
